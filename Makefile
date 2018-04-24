295132ef3932052078d2e7e1cf12848353c6ce40
Unknown <kabanos358@gmail.com>
2018-04-24 18:36:26 +0200

    Works partially

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
 CFLAGS=-Wall
-main: main.o bst.o read.o functions.o
+main: main.o libs/bst.o libs/read.o 
 
 clean:
-	rm -f main main.o bst.o read.o functions.o
+	rm -f main main.o libs/bst.o libs/read.o 
--- main.c
+++ main.c
@@ -1,22 +1,13 @@
-#include "bst.h"
-#include "read.h"
-#include "functions.h"
-
-struct list
-{
-	char *cmd;
-	struct list *next;
-};
+#include "libs/bst.h"
+#include "libs/read.h"
 
 struct node *root;
 struct node *current;
-struct list *head;
-struct list *curent;
 
-//void doSomething(int);
-//int doTime();
-//int math(int, int);
-//void parse(int);
+void doSomething(int);
+int doTime();
+int math(int, int);
+void parse(int);
 
 int *main(int argc, char *argv[])
 {
@@ -38,19 +29,119 @@ int *main(int argc, char *argv[])
 	printf("\n");
 	for (i = 1; i <= getCurrent_id(); i++)
 	{
-		doSomething(root, i);
+		doSomething(i);
 	}
-	int my_time = doTime();
 	printf("CurrentId: %d\n", getCurrent_id());
 
 	for (i = 1; i <= getCurrent_id(); i++)
 	{
+		int my_time = doTime();
 		current = getNode(root, i);
 		if (my_time > math(getHour(current), getMinutes(current)))
 			printf("Command outdated\n");
-		else
+		else{
 			printf("Command is about to be executed\n");
+			printf("Went to sleep for: %d", (math(getHour(current), getMinutes(current))-my_time)*60);
+			//sleep((math(getHour(current), getMinutes(current))-my_time)*60);
+			parse(i);
+		}		
 	}
-	parse(root, 2);
+	//parse(1);
 	return 0;
+}
+
+void doSomething(int i)
+{
+    struct node *current;
+    current = getNode(root, i);
+    if (current != NULL)
+    {
+        printf("%d, %d, %s, %d\n", getHour(current), getMinutes(current), getCommand(current), getInfo(current));
+    }
+}
+
+int doTime()
+{
+    time_t now;
+    struct tm ts;
+
+    now = time(NULL);
+
+    ts = *localtime(&now);
+
+    printf("%d:%d\n", ts.tm_hour, ts.tm_min);
+    int my_time = math(ts.tm_hour, ts.tm_min);
+
+    return my_time;
+}
+
+int math(int h, int m)
+{
+    return h * 60 + m;
+}
+
+void parse(int i)
+{
+    struct node *current;
+    current = getNode(root, i);
+    char *cmmd = getCommand(current);
+    int len = strlen(getCommand(current));
+    printf("%d, %s\n", len, cmmd);
+
+    char a[len];
+    strcpy(a, cmmd);
+
+    int j, k = 1, l = 0;
+    for (j = 0; j < len; j++)
+    {
+        if (a[j] == ' ')
+            k++;
+    }
+    printf("Number of elements: %d\n", k);
+
+    char *arr[k + 1];
+    char b[len + 1];
+    char *c;
+
+    k = 0;
+    for (j = 0; j < len; j++)
+    {
+        if (a[j] != ' ')
+        {
+            b[k] = a[j];
+            //printf("%c\n", b[k]);
+            k++;
+        }
+        else
+        {
+            b[k] = '\0';
+
+            c = malloc(sizeof(b));
+            strcpy(c, b);
+            arr[l] = c;
+
+            printf("Aktualne l: %d, Zawartość b: %s, Zawartość c: %s, Zawartość arr: %s\n", l, b, c, arr[l]);
+
+            k = 0;
+            l++;
+            memset(b, 0, len);
+        }
+    }
+    b[k] = '\0';
+    printf("test3\n");
+    c = malloc(sizeof(b));
+    strcpy(c, b);
+    arr[l] = c;
+    arr[l + 1] = 0;
+    
+    printf("%s%s%s\n",arr[0], arr[1], arr[2]);
+    arr[0]="ls"; arr[1]="-l"; arr[2]=0;
+    printf("%s%s%s\n",arr[0], arr[1], arr[2]);
+    
+
+    for (j = 0; j < sizeof(arr) / 8; j++)
+        printf("%d, %s\n", sizeof(arr) / 8, arr[j]);
+    memset(b, 0, len);
+    free(c);
+    execvp(arr[0], arr);
 }
